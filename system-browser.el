07aebf700ee27e1c5c114770f3b0f37748560ae4
Mariano Montone <marianomontone@gmail.com>
2021-07-26 21:32:30 -0300

    mode-line-format

--- system-browser.el
+++ system-browser.el
@@ -12,11 +12,26 @@
 (defgeneric sb:list-packages (system-browser-system))
 (defgeneric sb:list-categories (system-browser-system package))
 (defgeneric sb:list-definitions (system-browser-system package category))
+(defgeneric sb:packages-buffer-mode-line-format (system-browser-system))
+(defgeneric sb:categories-buffer-mode-line-format (system-browser-system))
+(defgeneric sb:definitions-buffer-mode-line-format (system-browser-system))
+(defgeneric sb:definition-buffer-mode-line-format (system-browser-system))
+
+;; Default mode-line
+(defmethod sb:packages-buffer-mode-line-format (system-browser-system)
+  "Packages")
+
+(defmethod sb:categories-buffer-mode-line-format (system-browser-system)
+  "Categories")
+
+(defmethod sb:definitions-buffer-mode-line-format (system-browser-system)
+  "Definitions")
+
+(defmethod sb:definition-buffer-mode-line-format (system-browser-system)
+  nil)
 
 (defmethod sb:list-packages ((system sb:common-lisp-system))
   (slime-eval '(cl:sort (cl:mapcar 'cl:package-name (cl:list-all-packages)) 'cl:string<)))
-
-(sb:list-packages (make-instance 'sb:common-lisp-system))
 
 (defvar sb:packages-buffer)
 (defvar sb:catgories-buffer)
@@ -25,75 +40,96 @@
 
 (defvar sb:current-browser-system (make-instance 'sb:common-lisp-system))
 
-(defun sb:open-packages-buffer ()
+(defgroup system-browser nil
+  "System browser configuration")
+
+(defun sb:create-packages-buffer ()
   (with-current-buffer "*sb-packages*"
+    (setq buffer-read-only nil)
+    (when (sb:packages-buffer-mode-line-format sb:current-browser-system)
+      (setq mode-line-format (sb:packages-buffer-mode-line-format sb:current-browser-system)))
+    (setq header-line-format mode-line-format)
+    (setq mode-line-format nil)
     (erase-buffer)
     (dolist (package-name (sb:list-packages sb:current-browser-system))
       (insert-button package-name
                      'action (lambda (btn)
                                (message package-name)
-                               (sb:open-categories-buffer package-name))
+                               (sb:create-categories-buffer package-name))
                      'follow-link t
                      'help-echo "Browse package")
-      (newline)))
+      (newline))
+    (setq buffer-read-only t))
   (wlf:select sb:wm 'packages))
 
-(defun sb:open-categories-buffer (package)
+(defun sb:create-categories-buffer (package)
   (with-current-buffer "*sb-categories*"
+    (setq buffer-read-only nil)
+    (when (sb:categories-buffer-mode-line-format sb:current-browser-system)
+      (setq mode-line-format (sb:categories-buffer-mode-line-format sb:current-browser-system)))
+    (setq header-line-format mode-line-format)
+    (setq mode-line-format nil)
     (erase-buffer)
     (insert package)
     (newline)
     (dolist (category (sb:list-categories sb:current-browser-system package))
       (insert-button category
                      'action (lambda (btn)
-                               (sb:open-definitions-buffer package category))
+                               (sb:create-definitions-buffer package category))
                      'follow-link t
                      'help-echo "Browse category")
-      (newline)))
+      (newline))
+    (setq buffer-read-only t))
   (wlf:select sb:wm 'categories))
 
-(defun sb:open-definitions-buffer (package category)
+(defun sb:create-definitions-buffer (package category)
   (with-current-buffer "*sb-definitions*"
+    (setq buffer-read-only nil)
+    (when (sb:definitions-buffer-mode-line-format sb:current-browser-system)
+      (setq mode-line-format (sb:definitions-buffer-mode-line-format sb:current-browser-system)))
+    (setq header-line-format mode-line-format)
+    (setq mode-line-format nil)
     (erase-buffer)
     (insert category)
     (newline)
     (dolist (definition (sb:list-definitions sb:current-browser-system package category))
       (insert-button definition
                      'action (lambda (btn)
-                               (sb:open-definition-buffer package category definition))
+                               (sb:create-definition-buffer package category definition))
                      'follow-link t
                      'help-echo "Browse definition")
-      (newline)))
+      (newline))
+    (setq buffer-read-only t))
   (wlf:select sb:wm 'definitions))
 
-(defun sb:open-definition-buffer (package category definition)
+(defun sb:create-definition-buffer (package category definition)
   (let ((definition-type
           (cond
            ((string= category "Functions") :function)
            ((string= category "Variables") :variable)
-	   ((string= category "Macros") :macro)
+           ((string= category "Macros") :macro)
            ((string= category "Classes") :class)))
         (definition-function
           (cond
            ((string= category "Functions") 'def-properties:function-properties)
            ((string= category "Variables") 'def-properties:variable-properties)
-	   ((string= category "Macros") 'def-properties:macro-properties)
+           ((string= category "Macros") 'def-properties:macro-properties)
            ((string= category "Classes") 'def-properties:class-properties))))
     (let ((definition-properties (slime-eval `(esb::serialize-for-emacs (,definition-function ',(make-symbol (concat package "::" definition)))))))
       (with-current-buffer "*sb-definition*"
-	(erase-buffer)
-	(let ((source (find :source definition-properties :key 'car)))
-	  (let ((file (cadr (find :file (remove-if-not 'listp source) :key 'car)))
-		(position (cadr (find :position (remove-if-not 'listp source) :key 'car))))
-	    (insert-file-contents file)
-	    (lisp-mode)
-	    (wlf:select sb:wm 'definition)
-	    ;; Assign file to buffer so changes in definition buffer can be saved
-	    (setq buffer-file-name file)
-	    ;; For some reason, sometimes definition buffer sets to read-only.
-	    ;; The following prevents that:
-	    (setq buffer-read-only nil)
-	    (goto-char position)))))))
+        (erase-buffer)
+        (let ((source (find :source definition-properties :key 'car)))
+          (let ((file (cadr (find :file (remove-if-not 'listp source) :key 'car)))
+                (position (cadr (find :position (remove-if-not 'listp source) :key 'car))))
+            (insert-file-contents file)
+            (lisp-mode)
+            (wlf:select sb:wm 'definition)
+            ;; Assign file to buffer so changes in definition buffer can be saved
+            (setq buffer-file-name file)
+            ;; For some reason, sometimes definition buffer sets to read-only.
+            ;; The following prevents that:
+            (setq buffer-read-only nil)
+            (goto-char position)))))))
 
 (defmethod sb:list-categories ((system sb:common-lisp-system) package)
   '("Variables" "Macros" "Functions" "Classes" "Generic Functions"))
@@ -103,14 +139,14 @@
           (cond
            ((string= category "Functions") :function)
            ((string= category "Variables") :variable)
-	   ((string= category "Macros") :macro)
+           ((string= category "Macros") :macro)
            ((string= category "Classes") :class))))
 
     (slime-eval `(esb::list-definitions ,package ,definition-type))))
 
 (defvar sb:wm)
 
-(defun open-system-browser ()
+(defun system-browser ()
   (interactive)
 
   (setq sb:packages-buffer (get-buffer-create "*sb-packages*"))
@@ -135,5 +171,5 @@
            (:name definition
                   :buffer "*sb-definition*")
            )))
-  (sb:open-packages-buffer)
+  (sb:create-packages-buffer)
   (wlf:select sb:wm 'packages))
